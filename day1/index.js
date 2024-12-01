882a7db03f27a66f1124415a78c31f7bb8c4b28d
puribey <pri.bey@gmail.com>
2024-12-01 19:41:01 +0000

    very horrible way day1

--- day1/index.js
+++ day1/index.js
@@ -3,8 +3,51 @@ const path = require("path");
 const fileTxt = fs.readFileSync(path.join(__dirname, "inputDayOne.txt"));
 const inputFromAdvent = fileTxt.toString();
 
-const main = () => {};
+const parseInput = (input) => {
+  const parsedInput = input
+    .trim()
+    .split("\n")
+    .reduce(
+      (acc, item) => {
+        const [first, second] = item.split(" ").filter(Boolean);
+        acc[0].push(Number(first));
+        acc[1].push(Number(second));
+        return acc;
+      },
+      [[], []]
+    );
 
-module.exports = {};
+  return parsedInput;
+};
 
-// main();
+const orderLists = (input) => {
+  const [first, second] = input;
+  const orderedFirst = first.sort((a, b) => a - b);
+  const orderedSecond = second.sort((a, b) => a - b);
+  return [orderedFirst, orderedSecond];
+};
+
+const getDistance = (input) => {
+  const distanceArray = [];
+  const [first, second] = orderLists(input);
+  for (let i = 0; i < first.length; i++) {
+    distanceArray.push(Math.abs(first[i] - second[i]));
+  }
+
+  return distanceArray;
+};
+
+const sumDistance = (input) => {
+  return input.reduce((acc, item) => acc + item, 0);
+};
+
+const main = () => {
+  const parsedInput = parseInput(inputFromAdvent);
+  const distance = getDistance(parsedInput);
+  const totalDistance = sumDistance(distance);
+  console.log(totalDistance);
+};
+
+module.exports = { parseInput, orderLists, getDistance, sumDistance };
+
+main();
--- day1/index.test.js
+++ day1/index.test.js
@@ -1,11 +1,31 @@
-const {} = require("./index");
+const { parseInput, orderLists, getDistance, sumDistance } = require("./index");
 
 const exampleInput = `
-
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
 `;
 
 describe("Day 1", () => {
-  test("Example 1", () => {
-    expect(true).toBe(true);
+  test("parseInput", () => {
+    expect(parseInput(exampleInput)).toEqual([
+      [3, 4, 2, 1, 3, 3],
+      [4, 3, 5, 3, 9, 3],
+    ]);
+  });
+  test("orderLists", () => {
+    expect(orderLists(parseInput(exampleInput))).toEqual([
+      [1, 2, 3, 3, 3, 4],
+      [3, 3, 3, 4, 5, 9],
+    ]);
+  });
+  test("getDistance", () => {
+    expect(getDistance(parseInput(exampleInput))).toEqual([2, 1, 0, 1, 2, 5]);
+  });
+  test("sumDistance", () => {
+    expect(sumDistance([2, 1, 0, 1, 2, 5])).toEqual(11);
   });
 });
