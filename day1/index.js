50ce265a92c536e6f726642ffc420bb2a4c8f2cd
puribey <pri.bey@gmail.com>
2024-12-01 18:34:54 +0000

    set new advent of code project

---
+++ day1/index.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const path = require("path");
+const fileTxt = fs.readFileSync(path.join(__dirname, "inputDayOne.txt"));
+const inputFromAdvent = fileTxt.toString();
+
+const main = () => {};
+
+module.exports = {};
+
+// main();
---
+++ day1/index.test.js
@@ -0,0 +1,11 @@
+const {} = require("./index");
+
+const exampleInput = `
+
+`;
+
+describe("Day 1", () => {
+  test("Example 1", () => {
+    expect(true).toBe(true);
+  });
+});
