50410f7bca53baecfa155b4954c34cb83795a64b
puribey <pri.bey@gmail.com>
2024-12-04 03:35:57 +0000

    FINALLY day 3 part 2

--- day3/index.js
+++ day3/index.js
@@ -10,6 +10,15 @@ const parseInput = (input) => {
   return matches.map((match) => match.slice(4, -1).split(",").map(Number));
 };
 
+const parseAndMultiplyInput = (input) => {
+  return input
+    .split("do()") // split into chunks
+    .map((chunk) => chunk.split("don't()")[0]) // remove don't()
+    .map((s) => [...s.matchAll(/mul\((\d+),(\d+)\)/g)]) // find all mul pairs
+    .flatMap((m) => m.map((m) => m[1] * m[2])) // multiply pairs
+    .reduce((acc, curr) => acc + curr, 0); // sum up
+};
+
 const multiplyPairsTotal = (pairs) => {
   return pairs.reduce((acc, pair) => acc + pair[0] * pair[1], 0);
 };
@@ -18,9 +27,11 @@ const main1 = () => {
   console.log(multiplyPairsTotal(parseInput(inputFromAdvent)));
 };
 
-const main2 = () => {};
+const main2 = () => {
+  console.log(parseAndMultiplyInput(inputFromAdvent));
+};
 
-module.exports = { parseInput, multiplyPairsTotal };
+module.exports = { parseInput, multiplyPairsTotal, parseAndMultiplyInput };
 
-main1();
-// main2();
+// main1();
+main2();
--- day3/index.test.js
+++ day3/index.test.js
@@ -1,9 +1,16 @@
-const { parseInput, multiplyPairsTotal } = require("./index");
+const {
+  parseInput,
+  multiplyPairsTotal,
+  parseAndMultiplyInput,
+} = require("./index");
 
 const exampleInput =
   "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
 
-describe("Day 3", () => {
+const exampleInput2 =
+  "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+
+describe("Day 3 Part 1", () => {
   test("parseInput", () => {
     expect(parseInput(exampleInput)).toEqual([
       [2, 4],
@@ -14,5 +21,11 @@ describe("Day 3", () => {
   });
   test("multiplyPairsTotal", () => {
     expect(multiplyPairsTotal(parseInput(exampleInput))).toEqual(161);
+  });
+});
+
+describe("Day 3 Part 2", () => {
+  test("parseAndMultiplyInput", () => {
+    expect(parseAndMultiplyInput(exampleInput2)).toEqual(48);
   });
 });
